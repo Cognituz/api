7216dd2d0da3a8349f1ea777b51a27cc2458ae99
Nicolas Oga <2112.oga@gmail.com>
2017-05-06 19:21:48 -0300

    Changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Watch out!, intended for use in production
-bundle exec rake db:migrate &&
-bundle exec rake db:seed &&
-bundle exec rake assets:clean &&
-bundle exec rake assets:precompile
+bundle exec rake \
+  db:create \
+  db:migrate \
+  db:seed \
+  assets:clean u\
+  assets:precompile

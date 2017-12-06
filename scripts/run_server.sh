ab4588d1319c7685fbf6e95222bdc14623fe728a
nahuelb <nahuel.becerra97@gmail.com>
2017-12-05 22:42:18 -0300

    try fixx staging

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Watch out!, intended for use in production
-rake setup &&
-bundle exec puma -e $RAILS_ENV -b unix://$SOCK_PATH 
+rake db:migrate &&
+bundle exec puma -e $RAILS_ENV -b unix://$SOCK_PATH
